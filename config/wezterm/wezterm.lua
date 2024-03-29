958f34a997454f20c833912d849622c7f4201441
erlenduh <erlend.uh@gmail.com>
2024-03-29 18:14:04 +0100

    changed theme and struchture of file

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,21 +1,23 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Config
-config.color_scheme = 'iTerm2 Smoooooth'
-config.font = wezterm.font_with_fallback({
-{ family = "JetBrains Mono", scale = 1.5 },
-})
+
+-- Hide tab
 config.hide_tab_bar_if_only_one_tab = true
 
--- and finally, return the configuration to wezterm
+-- Theme
+-- config.color_scheme = "iTerm2 Smoooooth"
+config.color_scheme = 'Molokai'
+
+-- Font
+config.font = wezterm.font_with_fallback({
+	{ family = "JetBrains Mono", scale = 1.5 },
+})
+
 return config
